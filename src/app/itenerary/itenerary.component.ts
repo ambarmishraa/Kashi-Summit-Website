0ebe9a2418ca55fecb3a340535c42ee7b8fe0e76
Aman Yadav <amanyddv@gmail.com>
2024-07-07 17:04:52 +0530

    updated

--- src/app/itenerary/itenerary.component.ts
+++ src/app/itenerary/itenerary.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-itenerary',
@@ -9,25 +15,20 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     trigger('rotateCard', [
       state('default', style({ transform: 'rotate(0deg)' })),
       state('rotated', style({ transform: 'rotate(-20deg)' })),
-      transition('default <=> rotated', [
-        animate('300ms ease-in-out')
-      ])
-    ])
-  ]
+      transition('default <=> rotated', [animate('300ms ease-in-out')]),
+    ]),
+  ],
 })
 export class IteneraryComponent {
   cards = [
-    { image: 'assets/itenary/Rectangle 41.svg' },
-    { image: 'assets/itenary/Rectangle 42.svg' },
-    { image: 'assets/itenary/party1.svg' },
-    { image: 'assets/itenary/Rectangle 41.svg' },
-    { image: 'assets/itenary/Rectangle 42.svg' },
-    { image: 'assets/itenary/party1.svg' }
+    { image: 'assets/itenary/DJ Night.jpeg', text: 'DJ Night' },
+    { image: 'assets/itenary/lasers show.jpeg', text: 'Laser Show' },
+    { image: 'assets/itenary/Rectangle 42.svg', text: 'Fun Night' },
+    { image: 'assets/itenary/party1.svg', text: 'Gala Dinner' },
   ];
   cardState = 'default';
 
   rotateCard() {
     this.cardState = this.cardState === 'default' ? 'rotated' : 'default';
   }
-
 }
