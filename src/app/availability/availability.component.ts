689bbf6661b476b9daf99e06199904b6ba0e3494
ambarmishraa <ambarmishra740@gmail.com>
2024-07-07 11:46:11 +0530

    slab done

--- src/app/availability/availability.component.ts
+++ src/app/availability/availability.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-availability',
   templateUrl: './availability.component.html',
-  styleUrl: './availability.component.scss'
+  styleUrls: ['./availability.component.scss']
 })
-export class AvailabilityComponent {
+export class AvailabilityComponent implements AfterViewInit {
+  @ViewChildren('card') cards!: QueryList<ElementRef>;
 
+  ngAfterViewInit(): void {
+    this.cards.forEach(card => {
+      card.nativeElement.addEventListener('mouseover', () => {
+        card.nativeElement.style.zIndex = '10';
+      });
+
+      card.nativeElement.addEventListener('mouseout', () => {
+        card.nativeElement.style.zIndex = '1';
+      });
+    });
+  }
 }
